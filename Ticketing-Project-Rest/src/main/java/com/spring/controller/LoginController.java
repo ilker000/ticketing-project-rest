d77610cafcef5d3392475877baf10f2f788263f3
ilker000 <ilkerdagdemir91@gmail.com>
2021-02-13 18:36:09 +0000

    Ticketing Project Rest

--- Ticketing-Project-Rest/src/main/java/com/spring/controller/LoginController.java
+++ Ticketing-Project-Rest/src/main/java/com/spring/controller/LoginController.java
@@ -1,19 +1,54 @@
 package com.spring.controller;
 
+import com.spring.dto.UserDTO;
+import com.spring.entity.ResponseWrapper;
+import com.spring.entity.User;
+import com.spring.entity.common.AuthenticationRequest;
+import com.spring.exception.TicketingProjectException;
+import com.spring.mapper.MapperUtil;
+import com.spring.service.UserService;
+import com.spring.util.JWTUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoginController {
 
-    @RequestMapping(value = {"/login","/"})
-    public String login() {
-        return "login";
+    private AuthenticationManager authenticationManager;
+    private UserService userService;
+    private MapperUtil mapperUtil;
+    private JWTUtil jwtUtil;
+
+    public LoginController(AuthenticationManager authenticationManager, UserService userService, MapperUtil mapperUtil, JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.mapperUtil = mapperUtil;
+        this.jwtUtil = jwtUtil;
     }
 
-    @RequestMapping("/welcome")
-    public String welcome() {
-        return "welcome";
+    @PostMapping("/authenticate")
+    public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest) throws TicketingProjectException {
+        String password = authenticationRequest.getPassword();
+        String username = authenticationRequest.getUsername();
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, password);
+        authenticationManager.authenticate(authentication);
+        UserDTO foundUser = userService.findByUserName(username);
+        User convertedUser = mapperUtil.convert(foundUser, new User());
+
+        if (!foundUser.isEnabled()){
+            throw new TicketingProjectException("Please verify your user");
+        }
+
+        String jwtToken = jwtUtil.generateToken(convertedUser);
+
+        return ResponseEntity.ok(new ResponseWrapper("Login successful", jwtToken));
     }
 
 }
---
+++ Ticketing-Project-Rest/src/main/java/com/spring/entity/common/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package com.spring.entity.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
