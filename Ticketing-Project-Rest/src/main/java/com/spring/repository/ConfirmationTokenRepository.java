d39e69523b27625ce73bb89716c4d69bf621eae1
ilker000 <ilkerdagdemir91@gmail.com>
2021-02-17 02:04:18 +0000

    Ticketing Project Rest

--- Ticketing-Project-Rest/src/main/java/com/spring/repository/ConfirmationTokenRepository.java
+++ Ticketing-Project-Rest/src/main/java/com/spring/repository/ConfirmationTokenRepository.java
@@ -2,9 +2,11 @@ package com.spring.repository;
 
 import com.spring.entity.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Integer> {
 
     Optional<ConfirmationToken> findByToken(String token);
